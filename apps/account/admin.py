4b53469e0a656ca4356c39b79b1eecf854039d57
Daydreamer_Z <shz1997@hotmail.com>
2022-03-31 22:19:39 +0800

    manage user page show related product

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from apps.account.models import Users, GithubUser
+from apps.product.models import Product
+
+
+class ProductInline(admin.TabularInline):
+    model = Product
 
 
 class UsersAdmin(admin.ModelAdmin):
     list_display = ("uid", "username", "nickname", "is_active", "is_staff", )
+    # 用户详细页面显示相关联产品信息(内联进来)
+    inlines = [ProductInline]
 
 
 admin.site.register(Users, UsersAdmin)

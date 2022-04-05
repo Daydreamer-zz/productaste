f405fdc2addc884d1b757f416c9e39cc84d1a2bc
Daydreamer_Z <shz1997@hotmail.com>
2022-04-05 12:48:43 +0800

    login page

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from apps.account.form import LoginForm
+
 
 # Create your views here.
+def login_view(request):
+    context = {}
+    if request.method == "GET":
+        context['form'] = LoginForm
+        return render(request, "login.html", context)
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+    return HttpResponse("<h1>Login Failed</h1>")
--- urls.py
+++ urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from apps.views import index_view
+from apps.account.views import login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view, name="home"),
+    path('', index_view, name="index"),
+    path('login/', login_view, name="login"),
 ]

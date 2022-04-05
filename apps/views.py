a383a12ee070d224a77bd753b394efbc4c95eab0
Daydreamer_Z <shz1997@hotmail.com>
2022-04-05 11:55:50 +0800

    loading more button

--- apps/views.py
+++ apps/views.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
+from datetime import timedelta
 from django.shortcuts import render
 from apps.product.models import Product
-from utils.time_utils import form_date
+from utils.time_utils import form_date, str2date
 
 
 def index_view(request):
-    products = {}
-    for i in range(3):
-        _date = form_date(i).date()
-        res = Product.objects.filter(public=True, create_at__contains=_date).order_by("-create_at", "-vote_count")
-        products[_date.strftime("%Y-%m-%d")] = res
-    return render(request, "index.html", {"products_dict": products})
+    last_dt = request.GET.get("last_dt", None)
+    if last_dt is None:
+        products = {}
+        for i in range(3):
+            _date = form_date(i).date()
+            res = Product.objects.filter(public=True, create_at__contains=_date).order_by("-create_at", "-vote_count")
+            products[_date.strftime("%Y-%m-%d")] = res
+        return render(request, "index.html", {"products_dict": products})
+    else:
+        _date = str2date(last_dt) + timedelta(-1)
+        products = Product.objects.filter(public=True, create_at__contains=_date).order_by("-create_at", "-vote_count")
+        context = {
+            "date": _date.strftime("%Y-%m-%d"),
+            "products": products
+        }
+        return render(request, "product_item.html", context)
--- utils/time_utils.py
+++ utils/time_utils.py
@@ -6,3 +6,7 @@ from datetime import timedelta
 
 def form_date(delta):
     return datetime.today() - timedelta(delta)
+
+
+def str2date(date, format="%Y-%m-%d"):
+    return datetime.strptime(date, format).date()
